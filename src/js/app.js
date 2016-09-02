94e19646d0d8afa61487b71de3ecc7e64d77e459
Alit4inov <alit4inov@gmail.com>
2016-09-02 22:23:13 +0300

    add google-map

--- src/js/app.js
+++ src/js/app.js
@@ -24,8 +24,6 @@ function initMobileNav() {
 		menuDrop: '.nav-drop'
 	});
 }
-
-
 /*
  * Simple Mobile Navigation
  */
@@ -131,3 +129,88 @@ function initMobileNav() {
 		});
 	};
 }(jQuery));
+
+// Google-Map
+// function initialize() {
+//   var myCenter=new google.maps.LatLng(43.600827, 1.441230);
+//   var mapProp = {
+//     center: myCenter,
+//     zoom:16,
+//     scrollwheel: false,
+//     mapTypeId:google.maps.MapTypeId.ROADMAP
+//   };
+//   var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+// }
+// google.maps.event.addDomListener(window, 'load', initialize);
+// End Google-map
+try {
+
+  var point = {
+    lat: 43.600827,
+    lng: 1.441230
+  };
+  var markerSize = {
+    x: 22,
+    y: 40
+  };
+
+
+  google.maps.Marker.prototype.setLabel = function(label) {
+    this.label = new MarkerLabel({
+      map: this.map,
+      marker: this,
+      text: label
+    });
+    this.label.bindTo('position', this, 'position');
+  };
+
+  var MarkerLabel = function(options) {
+    this.setValues(options);
+    this.span = document.createElement('span');
+    this.span.className = 'map-marker-label';
+  };
+
+  MarkerLabel.prototype = $.extend(new google.maps.OverlayView(), {
+    onAdd: function() {
+      this.getPanes().overlayImage.appendChild(this.span);
+      var self = this;
+      this.listeners = [
+        google.maps.event.addListener(this, 'position_changed', function() {
+          self.draw();
+        })
+      ];
+    },
+    draw: function() {
+      var text = String(this.get('text'));
+      var position = this.getProjection().fromLatLngToDivPixel(this.get('position'));
+      this.span.innerHTML = text;
+      this.span.style.left = (position.x - (markerSize.x / 2)) - (text.length * 3) + 85 + 'px';
+      this.span.style.top = (position.y - markerSize.y + 25) + 'px';
+    }
+  });
+
+  function initialize() {    
+    var myLatLng = new google.maps.LatLng(point.lat, point.lng);
+    var gmap = new google.maps.Map(document.getElementById('map_canvas'), {
+      zoom: 16,
+      center: myLatLng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      scrollwheel: false,
+      disableDefaultUI:true
+    });
+    var myMarker = new google.maps.Marker({
+      map: gmap,
+      position: myLatLng,
+      label: '7 Rue Clemence Isaure',
+      draggable: true
+    });
+
+  }
+
+  initialize();
+
+} catch (e) {
+  alert(e);
+}
+
